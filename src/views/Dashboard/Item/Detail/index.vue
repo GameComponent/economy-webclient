3a73ec5b7ffffde472ee87568ec414224eaadb08
Niels <nielsla95@gmail.com>
2019-03-03 17:41:37 +0100

    feat: added GiveItem to the item-detail page

--- src/views/Dashboard/Item/Detail/index.vue
+++ src/views/Dashboard/Item/Detail/index.vue
@@ -1,14 +1,32 @@
 <template>
   <div style="padding: 3rem;">
     <h1 style="margin-top: 0;">Item</h1>
-    <pre>{{ JSON.stringify(item, null, 2) }}</pre>
+
+    <!-- Give item -->
+    <div>
+      <h2>Give item</h2>
+      <div class="gc-wrapper-1">
+        <give-item :itemId="$route.params.id" />
+      </div>
+    </div>
+
+    <!-- Raw item -->
+    <div>
+      <h2>Raw item</h2>
+      <pre class="gc-pre">{{ JSON.stringify(item, null, 2) }}</pre>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import GiveItem from '@/components/GiveItem.vue';
 
-@Component
+@Component({
+  components: {
+    GiveItem,
+  },
+})
 export default class Item extends Vue {
   public item = null;
 
