333cca086d04d7334618323806447129094d7388
Niels <nielsla95@gmail.com>
2019-04-13 13:56:13 +0200

    feat: added logout page

---
+++ src/views/Dashboard/Logout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Logout extends Vue {
+  public mounted(): void {
+    localStorage.removeItem('token');
+    window.location.href = window.location.origin;
+  }
+}
+</script>
