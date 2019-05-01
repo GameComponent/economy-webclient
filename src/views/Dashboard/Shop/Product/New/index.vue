4bfe9c5d49afb289f6cc82db0b5d689b3c7bc3be
Niels <nielsla95@gmail.com>
2019-05-01 23:28:13 +0200

    improvement(shop): fixed indenting

--- src/views/Dashboard/Shop/Product/New/index.vue
+++ src/views/Dashboard/Shop/Product/New/index.vue
@@ -3,6 +3,39 @@
     <shop-header />
     <div class="p-16">
       <h1>New Product</h1>
+
+      <div class="mt-4">
+        <table>
+          <tr>
+            <td>Name:</td>
+            <td>
+              <input
+                v-model="product.name"
+                type="text"
+                class="gc-input"
+              >
+            </td>
+          </tr>
+
+          <tr>
+            <td></td>
+            <td>
+              <button @click="handleClickCreateProduct" class="gc-button">
+                Create
+              </button>
+            </td>
+          </tr>
+
+          <tr v-if="error">
+            <td></td>
+            <td>
+              <div class="bg-red-500 text-white rounded p-3">
+                {{ error }}
+              </div>
+            </td>
+          </tr>
+        </table>
+      </div>
     </div>
   </div>
 </template>
@@ -16,5 +49,29 @@ import ShopHeader from '../../components/ShopHeader.vue';
     ShopHeader,
   },
 })
-export default class ProductNew extends Vue {}
+export default class ProductNew extends Vue {
+  public product = {
+    name: '',
+  };
+
+  public error = null;
+
+  public handleClickCreateProduct(): void {
+    if (this.product.name.length === 0) {
+      alert('Please enter a product name.');
+      return;
+    }
+
+    this.$economyService.createProduct(this.product)
+      .then(({ product }) => {
+        console.log('createProduct result', product);
+        this.$router.push({
+          name: 'dashboard-shop-product-detail',
+          params: {
+            id: product.id,
+          },
+        });
+      });
+  }
+}
 </script>
