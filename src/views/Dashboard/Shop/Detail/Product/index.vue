de1dbf2ed56461670cfe66483ff3248c22df874b
Niels <nielsla95@gmail.com>
2019-05-01 23:32:15 +0200

    feat(shop): specific shop product detail

---
+++ src/views/Dashboard/Shop/Detail/Product/index.vue
@@ -0,0 +1,72 @@
+<style>
+  .product-header {
+    background-color: #dedfe0;
+    color: #4a4a4a;
+    text-transform: uppercase;
+    padding: 20px;
+    border: 1px solid #eee;
+    border-bottom: 0;
+    font-weight: 500;
+    letter-spacing: .05em;
+  }
+</style>
+
+<template>
+  <div>
+    <selected-shop-header />
+    <div class="p-16">
+      <h1>Shop Product</h1>
+
+      <div v-if="shop">
+        <div
+          v-for="product in shop.products"
+          :key="product.id"
+        >
+          <div class="product-header mt-4">
+            {{ product.name }}
+          </div>
+          <table class="gc-table gc-table--small-head text-left">
+            <thead>
+              <tr>
+                <th>Item ID</th>
+                <th>Name</th>
+                <th>Amount</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="productItem in product.items" :key="productItem.id">
+                <td>{{ productItem.item.id }}</td>
+                <td>{{ productItem.item.name }}</td>
+                <td>{{ productItem.amount }}</td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import SelectedShopHeader from '../../components/SelectedShopHeader.vue';
+import { V1Shop } from '@/../vendor/economy-client/api.ts';
+
+@Component({
+  components: {
+    SelectedShopHeader,
+  },
+})
+export default class ShopProductDetail extends Vue {
+   public shop: V1Shop = null;
+
+  public mounted() {
+    this.$economyService.getShop(this.$route.params.id)
+      .then(({ shop }) => {
+        this.shop = shop;
+      });
+  }
+}
+</script>
